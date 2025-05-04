8cf0460b6ece2df5baed44054c43a874385ebe0b
PC\eyerj <joel.eyer@bwz-rappi.ch>
2025-05-04 18:59:23 +0200

    create new diagrams and documentation

--- DB/CopyImagesToDb.ps1
+++ DB/CopyImagesToDb.ps1
@@ -1,9 +1,5 @@
-﻿# Chnage the Folders
-$zielOrdner = "C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\Image\"
-$quelleOrdner = "C:\IMS-T4_23\Praktikum\CharacterConfigurator\CharacterConfigurator\Resources\Image\"
+﻿# Chanage the Folders
+$destinationFolder = "C:\ProgramData\MySQL\MySQL Server 8.4\Uploads\Image\"
+$sourceFolder = "\ProjektZuehlke\CharacterConfigurator\CharacterConfigurator\Resources\Image"
 
-if (-Not (Test-Path -Path $zielOrdner)) {
-    New-Item -Path $zielOrdner -ItemType Directory
-}
-
-Copy-Item -Path "$quelleOrdner*" -Destination $zielOrdner -Recurse -Force
+Copy-Item -Path "$sourceFolder*" -Destination $destinationFolder -Recurse -Force
